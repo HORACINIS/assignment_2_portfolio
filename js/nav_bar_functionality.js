925c93ce0de5fc6fe2c689c891375a2a4d6b0255
Horacinis <horacinis@gmail.com>
2020-09-01 10:46:39 +1000

    structured the skills page with title  images and paragraphs (all with placeholder text for now)

--- js/nav_bar_functionality.js
+++ js/nav_bar_functionality.js
@@ -1,15 +1,16 @@
 const menu_bar = document.querySelector('.menu');
 
-        menu_bar.addEventListener('click', (event) => {
-            if (event.target.className === 'bar_menu') {
+menu_bar.addEventListener('click', (event) => {
+    if (event.target.className === 'menu_bar' || event.target.className === 'toggle') {
 
-                for (let i = 0; i < menu_bar.children.length; i++) {
-                    let li = menu_bar.children[i];
-                    if (li.className === 'nav_item active') {
-                        li.classList.remove('active');
-                    } else {
-                        li.classList.add('active');
-                    }
-                }
+        for (let i = 0; i < menu_bar.children.length; i++) {
+            let li = menu_bar.children[i];
+            if (li.className === 'nav_item active') {
+                li.classList.remove('active');
+            } else {
+                li.classList.add('active');
             }
-        });
+        }
+        console.log(event.target);
+    }
+});
