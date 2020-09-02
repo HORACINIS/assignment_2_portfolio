60b3bae1a921698a6a84612f836c71afaa16f6e4
Horacinis <horacinis@gmail.com>
2020-09-02 10:28:48 +1000

    added indentation to js file

--- js/nav_bar_functionality.js
+++ js/nav_bar_functionality.js
@@ -1,15 +1,15 @@
 const menu_bar = document.querySelector('.menu');
 
-        menu_bar.addEventListener('click', (event) => {
-            if (event.target.className === 'menu_bar' || event.target.className === 'toggle' || event.target.className === 'toggle active') {
+menu_bar.addEventListener('click', (event) => {
+    if (event.target.className === 'menu_bar' || event.target.className === 'toggle' || event.target.className === 'toggle active') {
 
-                for (let i = 0; i < menu_bar.children.length; i++) {
-                    let li = menu_bar.children[i];
-                    if (li.className === 'nav_item active') {
-                        li.classList.remove('active');
-                    } else {
-                        li.classList.add('active');
-                    }
-                }
+        for (let i = 0; i < menu_bar.children.length; i++) {
+            let li = menu_bar.children[i];
+            if (li.className === 'nav_item active') {
+                li.classList.remove('active');
+            } else {
+                li.classList.add('active');
             }
-        });
+        }
+    }
+});
