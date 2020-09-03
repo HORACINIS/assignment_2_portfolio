53144c85c45493739eacb84748e537843746d005
horacinis <horacinis@gmail.com>
2020-09-03 16:00:44 +1000

    Made the images in the blog posts smaller

--- js/nav_bar_functionality.js
+++ js/nav_bar_functionality.js
@@ -1,5 +1,11 @@
-const menu_bar = document.querySelector('.menu');
 
+/*Giving the hamburger menu functionality
+If clicked on then the 'active' class is added to the list items and their display property 
+is changed from none to display.
+If the menu items already have the 'active" class then this will be removed making the menu 
+list items disappear again by changing the display property to none.
+*/
+const menu_bar = document.querySelector('.menu');
 menu_bar.addEventListener('click', (event) => {
     if (event.target.className === 'menu_bar' || event.target.className === 'toggle' || event.target.className === 'toggle active') {
 
